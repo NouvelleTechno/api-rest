e58bca2d1c4b0047b957f851daa3f49f7107932f
Nicolas Malet <alternative.rvb@gmail.com>
2021-10-30 23:24:08 +0200

    Problème sur la requête SQL créer

--- models/Produits.php
+++ models/Produits.php
@@ -49,7 +49,7 @@ class Produits{
     public function creer(){
 
         // Ecriture de la requête SQL en y insérant le nom de la table
-        $sql = "INSERT INTO " . $this->table . " SET nom=:nom, prix=:prix, description=:description, categories_id=:categories_id, created_at=:created_at";
+        $sql = "INSERT INTO " . $this->table . " (nom, prix, description, categories_id) VALUES (:nom, :prix, :description, :categories_id)";
 
         // Préparation de la requête
         $query = $this->connexion->prepare($sql);
@@ -59,14 +59,12 @@ class Produits{
         $this->prix=htmlspecialchars(strip_tags($this->prix));
         $this->description=htmlspecialchars(strip_tags($this->description));
         $this->categories_id=htmlspecialchars(strip_tags($this->categories_id));
-        $this->created_at=htmlspecialchars(strip_tags($this->created_at));
 
         // Ajout des données protégées
         $query->bindParam(":nom", $this->nom);
         $query->bindParam(":prix", $this->prix);
         $query->bindParam(":description", $this->description);
         $query->bindParam(":categories_id", $this->categories_id);
-        $query->bindParam(":created_at", $this->created_at);
 
         // Exécution de la requête
         if($query->execute()){
